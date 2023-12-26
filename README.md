d659e466d15237af6d951389c255e00c56276bd0
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2023-12-26 19:36:20 +0200

    updaded the style, of the hero

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Link: https://my-data-portfolio-project.netlify.app/
 This is my protfolio project with links to other projects
 
 TODO
-- [ ] fix sizing issue, when in mobile version, first project is wider
-- [ ] remove Bootstrap
+- [x] fix sizing issue, when in mobile version, first project is wider
+- [x] remove Bootstrap
+- [x] organaize the css and html  code with comments
 - [ ] redo all the CSS in index.css
 - [ ] redo most of the HTML in index.html
 - [ ] redo the menu in menu.html, without Bootstrap
+- [ ] add whatsapp floating window at the bottom
+- [x] add a single evenListener
---
+++ slider.js
@@ -0,0 +1,63 @@
+document.addEventListener("click", function(e) {
+    switch (e.target.className) {
+        case "next-btn":
+            changeSlide(1)
+            break
+        case "fa-solid fa-chevron-right":
+            changeSlide(1)
+            break
+        case "prev-btn":
+            changeSlide(-1)
+            break
+        case "fa-solid fa-chevron-left":
+            changeSlide(-1)
+            break
+        case "dot":
+            currentSlide()
+            break
+        default:
+            break
+        }
+})
+    
+    let slideIndex = 1
+    
+function showSlides(index=slideIndex += 1) {
+    const slides = document.getElementsByClassName("slides")
+    const dots = document.getElementsByClassName("line")
+    
+    if (index > slides.length) {
+        slideIndex = 1
+    } else if (index < 1) {
+        slideIndex = slides.length
+    }
+
+    let i
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block"
+    dots[slideIndex - 1].className += " active"
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += 1)
+}
+
+function currentSlide() {
+    showSlides()
+}
+    
+function autoSlide() {
+    changeSlide(2)
+    setTimeout(autoSlide, 8000)
+}
+
+showSlides()
+// autoSlide()
+
