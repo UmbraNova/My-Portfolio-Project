efc316a14d4d0959a0a04ff9417d63b41b356398
UmbraNova <alexandru.nedelcu.office@gmail.com>
2024-01-03 13:43:54 +0200

    moved footer into file, fixed features section sizing

--- common.js
+++ common.js
@@ -9,4 +9,16 @@ function includeMenu() {
   xhttp.send()
 }
 
+function includeFooter() {
+  var xhttp = new XMLHttpRequest()
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("footer-container").innerHTML = this.responseText
+    }
+  }
+  xhttp.open("GET", "footer.html", true)
+  xhttp.send()
+}
+
 includeMenu();
+includeFooter();
