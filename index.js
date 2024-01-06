73457f65484e22e9068b2d3ae396a0e3494ffac6
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2024-01-06 22:10:26 +0200

    added test elemtn for contact btn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-// const menu = document.querySelector("#mobileMenu")
-// const menuLinks = document.querySelector(".navbarMenu")
 
-// menu.addEventListener("click", function() {
-//     menu.classList.toggle("is-active");
-//     menuLinks.classList.toggle("active");
-// })
+
+document.body.style.overflow = "hidden"
+
+let contactEl = ""
+// setTimout to load it after everything else
+setTimeout(function() {
+    contactEl = document.getElementById("contact-el")
+}, 10)
+
+
+document.addEventListener("click", function(e){
+    if (e.target.tagName=="A") {
+        document.getElementById("menu-check").checked = false
+    } else if (e.target.id=="exit-btn") {
+        contactEl.style.display = "none"
+    }
+
+    if (e.target.id=="contact-me-btn") {
+        contactEl.style.display = "flex"
+    }
+  })
