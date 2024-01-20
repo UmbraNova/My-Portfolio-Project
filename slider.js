a50fb10bcb26fbf61594cd053fef22ee5eb39b2e
Alexandru Nedelcu <alexandru.nedelcu.office@gmail.com>
2024-01-20 06:06:07 +0200

    added inverted arrow buttons

--- slider.js
+++ slider.js
@@ -1,9 +1,9 @@
 document.addEventListener("click", function(e) {
 
-    const sliderBtn = e.target.className        
-    if (sliderBtn == "next-btn" || sliderBtn == "fa-solid fa-chevron-right") {
+    const sliderBtn = e.target.className
+    if (sliderBtn == "next-btn invert" || sliderBtn == "fa-solid fa-chevron-right") {
         showSlides(slideIndex += 1)
-    } else if (sliderBtn == "prev-btn" || sliderBtn == "fa-solid fa-chevron-left") {
+    } else if (sliderBtn == "prev-btn invert" || sliderBtn == "fa-solid fa-chevron-left") {
         showSlides(slideIndex += -1)
     } else if (sliderBtn == "line one") {
         showSlides(slideIndex = 1)
@@ -14,10 +14,14 @@ document.addEventListener("click", function(e) {
     }
 
 })
+
     
 let slideIndex = 0
 let wasBtnClicked = false
-let sliderTimeout;
+let sliderTimeout
+const nextArrowBtn = document.querySelector("#next-btn")
+const prevArrowBtn = document.querySelector("#prev-btn")
+
 
 function showSlides(index=slideIndex += 1, btnClicked=true) {
     // btnClicked => autoSlider is sending false, when user clicks it defaults to true
@@ -45,14 +49,29 @@ function showSlides(index=slideIndex += 1, btnClicked=true) {
 
     slides[slideIndex - 1].style.display = "block"
     lines[slideIndex - 1].className += " active"
+
+    if (slideIndex === 1) {
+        invertArrowColors()
+    } else (
+        invertArrowColorsBack()
+    )
 }
 
+function invertArrowColors() {
+    prevArrowBtn.style.filter = "invert()"
+    nextArrowBtn.style.filter = "invert()"
+}
+
+function invertArrowColorsBack() {
+    prevArrowBtn.style.filter = "none"
+    nextArrowBtn.style.filter = "none"
+}
 
 function autoSlider() {
     sliderTimeout = window.setTimeout(() => {
         showSlides(slideIndex += 1, false)
         autoSlider()
-    }, 2500)
+    }, 3500)
 }
 
 function startSliderAfterClick() {
